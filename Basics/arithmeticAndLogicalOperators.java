8c502f799c71010d62ba0bf87de8c38eab8a71ea
bzsol <bereczzsolt2000@gmail.com>
2023-12-29 20:16:54 +0100

    added aritmetic and logical operators

--- Basics/arithmeticAndLogicalOperators.java
+++ Basics/arithmeticAndLogicalOperators.java
@@ -1,6 +1,19 @@
 package Basics;
 public class arithmeticAndLogicalOperators{
     public static void main(String[] args) {
-        
+        // Aritmetic operators in Java ~ It's similar like in C
+        System.out.println(10 + 10);
+        System.out.println(50 - 25);
+        System.out.println(5 * 6);
+        System.out.println(70 / 35);
+        System.out.println(10 % 3);
+
+        // Logical operators
+        System.out.println(1 == 3);
+        System.out.println(5 < 6);
+        System.out.println(9 > 3);
+        System.out.println(5 >= 8);
+        System.out.println(9 <= 6);
+        System.out.println(5 != 4);
     }
 }
--- Basics/conditions.java
+++ Basics/conditions.java
@@ -1,4 +1,22 @@
 package Basics;
+
+import java.util.Scanner;
+
 public class conditions {
+    // if condition
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int age = scanner.nextInt();
+    scanner.close();
+    if (age < 18){
+        System.err.println("You are underage");
+    }
+    else if(age > 18 && age < 65){
+        System.out.println("You are an adult");
+    }
+    else{
+        System.out.println("You are retired and lived enough");
+    }
+    }
     
 }
