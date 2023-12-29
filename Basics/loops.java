5006f1de553201be31765a5f701e1d37db676222
bzsol <bereczzsolt2000@gmail.com>
2023-12-29 19:58:49 +0100

    Added loops

--- Basics/loops.java
+++ Basics/loops.java
@@ -1,4 +1,21 @@
 package Basics;
 public class loops {
-    
+    // while loop
+    public static void main(String[] args) {
+    int i = 0;
+    while(i < 10){
+        System.out.println("ITS Java Course");
+        i++;
+    }
+    // for loop in Java
+    for(int j=0;j<10;j++){
+        System.out.println("Hello ITS Java Course");
+    }
+
+    // Foreach is a really good way to loop throuugh an array or some kind of dataset like a String
+    String s = "instantcoffee";
+    for (char c : s.toCharArray()) {
+        System.err.print(c + " ");
+    }
+    }
 }
